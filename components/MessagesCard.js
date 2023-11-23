9d57952c49401c49b838cf81022d3ba8d20906a8
chainworks <virlanchainworks@gmail.com>
2023-11-23 11:56:25 +0800

    message tab can now post messages between client and bidder and will show up the message thread

--- components/MessagesCard.js
+++ components/MessagesCard.js
@@ -1,15 +1,62 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput } from 'react-native';
 
 const MessagesCard = ({ user, jobad, title, content }) => {
 
     console.log(`MessagesCard ${JSON.stringify(user)}::${user.user.userid}::${user.bids}`)
     const [showMoreInfo, setShowMoreInfo] = useState(false);
+    const [replyModalVisible, setReplyModalVisible] = useState(false);
+    const [replyMessage, setReplyMessage] = useState('');
+    // Set the necessary information in state
+    const [replybidid, setReplyBidId] = useState(null);
+    const [replybidderid, setReplyBidderId] = useState(null);
+    const [replybidoid, setReplyBidOid] = useState(null)
 
     const handleCardPress = () => {
         // Implement the logic to show more information
         console.log(`Message Card tapped! More info for: ${title}`);
         setShowMoreInfo(!showMoreInfo);
+    };
+
+    const handleReplyPress = (bid) => {
+      // Set the necessary information in state
+      setReplyBidId(bid._id);
+      setReplyBidderId(bid.bidderId);
+      setReplyBidOid(jobad._id);
+      setReplyModalVisible(true);
+    };
+
+    const handleSendMessage = () => {
+      // Construct the endpoint URL based on jobad and current user
+      //const endpoint = `http://localhost:3000/job/${jobad._id.$oid}/bid/${user.user.userid}/bidder-message`;
+      const endpoint = `http://localhost:3000/job/${replybidoid}/bid/${replybidid}/message`;
+      // Construct the request body
+      const requestBody = {
+        senderId: user.user.userid,
+        receiverId: replybidderid,
+        message: replyMessage,
+      };
+      console.log(`sendinginfo ${replybidoid}::${replybidderid}`)
+      //console.log(`sendinginfo ${jobad._id.$oid}/bid/${jobad._id.$oid}::${senderId}::${receiverId}::${message}`)
+      // Send the message using fetch or your preferred method
+      fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Handle success or error
+          console.log('Reply sent:', data);
+  
+          // Close the modal
+          setReplyModalVisible(false);
+        })
+        .catch((error) => {
+          console.error('Error sending reply:', error);
+        });
     };
 
   return (
@@ -28,13 +75,17 @@ const MessagesCard = ({ user, jobad, title, content }) => {
 
                 {/* Display messages for the bidder */}
                 {bid.messages && bid.messages.length > 0 && (
-                  <View>
+                  <View style={{ marginBottom:20}}>
                     <Text>Messages:</Text>
                     {bid.messages.map((message) => (
                       <View key={message._id.$oid}>
                         <Text>{message.senderId.$oid}</Text>
                         <Text>{message.message}</Text>
-                        {/* Add more message information as needed */}
+                        
+                        {/* Add a reply button */}
+                        <TouchableOpacity onPress={()=>handleReplyPress(bid)}>
+                          <Text>Reply</Text>
+                        </TouchableOpacity>
                       </View>
                     ))}
                   </View>
@@ -44,6 +95,28 @@ const MessagesCard = ({ user, jobad, title, content }) => {
           </View>
         )}
       </View>
+
+      {/* Modal for replying to a message */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={replyModalVisible}
+        onRequestClose={() => setReplyModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <TextInput
+              style={styles.input}
+              placeholder="Type your reply..."
+              onChangeText={(text) => setReplyMessage(text)}
+              value={replyMessage}
+            />
+            <TouchableOpacity style={styles.modalButton} onPress={handleSendMessage}>
+              <Text>Send Reply</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </TouchableOpacity>
   );
 };
@@ -74,6 +147,32 @@ const styles = StyleSheet.create({
   budget: {
     fontSize: 14,
     marginBottom: 8,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+    alignItems: 'center',
+  },
+  input: {
+    fontSize: 16,
+    marginBottom: 20,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+  },
+  modalButton: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
   },
 });
 
