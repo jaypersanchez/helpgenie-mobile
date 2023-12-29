203a4ef6185da77d714dc41592ce0ac402c5a2f5
romar <romar.doinog@gmail.com>
2023-12-29 11:49:08 +0800

    merge fix

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
     plugins: [
       [
         "module-resolver",
@@ -15,20 +15,11 @@ module.exports = function (api) {
           extensions: [".js", ".jsx", ".ts", ".tsx"],
         },
       ],
-    ],
-  };
-};
-
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
-    plugins: [
       [
-        'module:react-native-dotenv',
+        "module:react-native-dotenv",
         {
-          moduleName: '@env',
-          path: '.env',
+          moduleName: "@env",
+          path: ".env",
           blacklist: null,
           whitelist: null,
           safe: false,
